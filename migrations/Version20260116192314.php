11cff3a09049e17d70b1fa7d4381f03d8caa9341
_Duck-Noris_ <rayan.boughalia@gmail.com>
2026-01-16 20:32:04 +0100

    fixed: data entity got the correct columns and note to myself the aquarium_id is at the last column because its a foreign key

--- migrations/Version20260116192314.php
+++ migrations/Version20260116192314.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260115110311 extends AbstractMigration
+final class Version20260116192314 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,7 +21,7 @@ final class Version20260115110311 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE aquarium (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, capacity DOUBLE PRECISION NOT NULL, type VARCHAR(255) NOT NULL, user_id INT NOT NULL, INDEX IDX_2BBA6EB2A76ED395 (user_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('CREATE TABLE data (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, value DOUBLE PRECISION NOT NULL, created_at DATETIME NOT NULL, aquarium_id INT NOT NULL, INDEX IDX_ADF3F3637051F3DE (aquarium_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE data (id INT AUTO_INCREMENT NOT NULL, created_at DATETIME NOT NULL, temp DOUBLE PRECISION NOT NULL, ph DOUBLE PRECISION NOT NULL, kh DOUBLE PRECISION NOT NULL, gh DOUBLE PRECISION NOT NULL, cl2 DOUBLE PRECISION NOT NULL, no2 DOUBLE PRECISION NOT NULL, no3 DOUBLE PRECISION NOT NULL, observation VARCHAR(255) NOT NULL, aquarium_id INT NOT NULL, INDEX IDX_ADF3F3637051F3DE (aquarium_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE fish (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, aquarium_id INT NOT NULL, INDEX IDX_3F7444337051F3DE (aquarium_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE setting (id INT AUTO_INCREMENT NOT NULL, code VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, user_id INT NOT NULL, INDEX IDX_9F74B898A76ED395 (user_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
--- src/Entity/Data.php
+++ src/Entity/Data.php
@@ -10,49 +10,127 @@ class Data
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(nullable: false)]
+    private int $id = 0;
 
-    #[ORM\Column(length: 255)]
-    private ?string $type = null;
+    #[ORM\Column(nullable: false)]
+    private \DateTimeImmutable $createdAt;
 
-    #[ORM\Column]
-    private ?float $value = null;
-
-    #[ORM\Column]
-    private ?\DateTimeImmutable $createdAt = null;
-
-    #[ORM\ManyToOne(inversedBy: 'data')]
+    #[ORM\ManyToOne(targetEntity: Aquarium::class)]
     #[ORM\JoinColumn(nullable: false)]
-    private ?Aquarium $aquarium = null;
+    private $aquarium;
+
+    #[ORM\Column(nullable: false)]
+    private float $temp = 26.0;
+
+    #[ORM\Column(nullable: false)]
+    private float $ph = 7.0;
+
+    #[ORM\Column(nullable: false)]
+    private float $kh = 4.0;
+
+    #[ORM\Column(nullable: false)]
+    private float $gh = 8.0;
+
+    #[ORM\Column(nullable: false)]
+    private float $cl2 = 0.0;
+
+    #[ORM\Column(nullable: false)]
+    private float $no2 = 0.0;
+
+    #[ORM\Column(nullable: false)]
+    private float $no3 = 0.0;
+
+    #[ORM\Column(nullable: false)]
+    private string $observation = '';
+
+    public function getObservation(): ?string
+    {
+        return $this->observation;
+    }
+
+    public function setObservation(?string $observation): static
+    {
+        $this->observation = $observation;
+
+        return $this;
+    }
+
+    public function getKh(): ?float
+    {
+        return $this->kh;
+    }
+
+    public function setKh(?float $kh): static
+    {
+        $this->kh = $kh;
+
+        return $this;
+    }
+
+    public function getGh(): ?float
+    {
+        return $this->gh;
+    }
+
+    public function setGh(?float $gh): static
+    {
+        $this->gh = $gh;
+
+        return $this;
+    }
+
+    public function getPh(): ?float
+    {
+        return $this->ph;
+    }
+
+    public function setPh(?float $ph): static
+    {
+        $this->ph = $ph;
+
+        return $this;
+    }
+
+    public function getCl2(): ?float
+    {
+        return $this->cl2;
+    }
+
+    public function setCl2(?float $cl2): static
+    {
+        $this->cl2 = $cl2;
+
+        return $this;
+    }
+
+    public function getNo2(): ?float
+    {
+        return $this->no2;
+    }
+
+    public function setNo2(?float $no2): static
+    {
+        $this->no2 = $no2;
+
+        return $this;
+    }
+
+    public function getNo3(): ?float
+    {
+        return $this->no3;
+    }
+
+    public function setNo3(?float $no3): static
+    {
+        $this->no3 = $no3;
+
+        return $this;
+    }
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getType(): ?string
-    {
-        return $this->type;
-    }
-
-    public function setType(string $type): static
-    {
-        $this->type = $type;
-
-        return $this;
-    }
-
-    public function getValue(): ?float
-    {
-        return $this->value;
-    }
-
-    public function setValue(float $value): static
-    {
-        $this->value = $value;
-
-        return $this;
     }
 
     public function getCreatedAt(): ?\DateTimeImmutable
@@ -75,6 +153,18 @@ class Data
     public function setAquarium(?Aquarium $aquarium): static
     {
         $this->aquarium = $aquarium;
+
+        return $this;
+    }
+
+    public function getTemp(): ?float
+    {
+        return $this->temp;
+    }
+
+    public function setTemp(?float $temp): static
+    {
+        $this->temp = $temp;
 
         return $this;
     }
